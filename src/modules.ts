926c1f4f663a154a03dfe6bb04de2b8f8b2b73eb
Igor Savin <igor.savin@lokalise.com>
2025-08-27 17:01:52 +0300

    Register missing module

--- src/modules.ts
+++ src/modules.ts
@@ -1,4 +1,8 @@
 import type { AbstractModule } from 'opinionated-machine'
 import { CommonModule } from './infrastructure/CommonModule.ts'
+import { ConnectorModule } from './modules/ConnectorModule.js'
 
-export const ALL_MODULES: readonly AbstractModule<unknown>[] = [new CommonModule()]
+export const ALL_MODULES: readonly AbstractModule<unknown>[] = [
+  new CommonModule(),
+  new ConnectorModule(),
+]
